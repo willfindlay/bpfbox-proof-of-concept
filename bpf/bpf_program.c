be72cad23293db3459fef67a1cc46845bff3bacf
willfindlay <william@williamfindlay.com>
2020-05-07 21:03:59 -0400

    Skeleton for BPF program

---
+++ bpf/bpf_program.c
@@ -0,0 +1,9 @@
+TRACEPOINT_PROBE(raw_syscalls, sys_enter)
+{
+    return 0;
+}
+
+TRACEPOINT_PROBE(raw_syscalls, sys_exit)
+{
+    return 0;
+}
--- bpf/bpf_program.py
+++ bpf/bpf_program.py
@@ -1,6 +1,25 @@
+import sys
+import signal
+import atexit
+import time
+
 from bcc import BPF
 
-text = r"""
-"""
+def load_program():
+    with open('bpf_program.c', 'r') as f:
+        text = f.read()
+    return BPF(text=text)
 
-b = BPF(text=text)
+def cleanup():
+    pass
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, lambda x, y: sys.exit())
+    signal.signal(signal.SIGTERM, lambda x, y: sys.exit())
+
+    atexit.register(cleanup)
+
+    b = load_program()
+
+    while 1:
+        time.sleep(0.1)
