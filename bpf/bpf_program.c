5c19c1eff6f7908616e27798b84035073a1814f6
willfindlay <william@williamfindlay.com>
2020-05-08 11:25:16 -0400

    More BPF skeleton

--- bpf/bpf_program.c
+++ bpf/bpf_program.c
@@ -1,5 +1,57 @@
+#include <linux/limits.h>
+
+typedef struct
+{
+    u8 tainted;
+}
+bprm_state;
+
+static int bpf_strncmp(char *s1, char *s2, u32 n)
+{
+    int mismatch = 0;
+    for (int i = 0; i < n && i < sizeof(s1) && i < sizeof(s2); i++)
+    {
+        if (s1[i] != s2[i])
+            return s1[i] - s2[i];
+
+        if (s1[i] == s2[i] == '\0')
+            return 0;
+    }
+
+    return 0;
+}
+
+static int bpf_strcmp(char *s1, char *s2)
+{
+    u32 s1_size = sizeof(s1);
+    u32 s2_size = sizeof(s2);
+
+    return bpf_strncmp(s1, s2, s1_size < s2_size ? s1_size : s2_size);
+}
+
+RAW_TRACEPOINT_PROBE(sched_process_exec)
+{
+    u32 pid = bpf_get_current_pid_tgid();
+
+    /* Figure out if we are in webserver.py */
+    // TODO
+
+    /* Create / look up process */
+    // TODO
+
+    /* Yoink the linux_binprm */
+    struct linux_binprm *bprm = (struct linux_binprm *)ctx->args[2];
+
+    return 0;
+}
+
 TRACEPOINT_PROBE(raw_syscalls, sys_enter)
 {
+    char comm[16];
+    bpf_get_current_comm(comm, sizeof(comm));
+    if (!bpf_strcmp(comm, "webserver.py"))
+        bpf_trace_printk("ls!\n");
+
     return 0;
 }
 
